953dee69e9550b9144844ba934243b7764381561
Juzonb1r <zh.karaev@yahoo.com>
2025-11-23 00:54:51 -0600

    Change RDS password retrieval to use secret string
    Updated RDS password retrieval method to use the secret string directly.

--- infra/terraform/rds.tf
+++ infra/terraform/rds.tf
@@ -38,7 +38,7 @@ resource "aws_db_instance" "orders_db" {
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   username                = var.rds_username
-  password                = jsondecode(data.aws_secretsmanager_secret_version.rds_password_version.secret_string)["password"]
+  password                = data.aws_secretsmanager_secret_version.rds_password_version.secret_string
   db_subnet_group_name    = aws_db_subnet_group.orders.name
   vpc_security_group_ids  = [aws_security_group.rds.id]
   publicly_accessible     = false
