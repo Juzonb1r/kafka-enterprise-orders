23de9d9423d7d534b42520ec298196c43ffadad0
Your Name <you@example.com>
2025-12-01 23:03:57 -0600

    Added new files and folders, updated structure

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,14 @@
+backend "s3" {
+    # ✅ ASSUMPTION: You already have this bucket in us-east-2:
+    #   kafka-enterprise-orders-tfstate
+    bucket         = "kafka-enterprise-orders-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+
+    # ✅ You showed this DynamoDB table in us-east-2:
+    #   kafka-enterprise-orders-lock
+    dynamodb_table = "kafka-enterprise-orders-lock"
+
+    encrypt = true
+  }
+}
